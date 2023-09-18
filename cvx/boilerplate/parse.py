9f18dedec2cd10f2d38fa6a9fcc83bc33a1d35e6
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-09-17 21:17:55 -0700

    GitHub machinery

--- cvx/boilerplate/parse.py
+++ cvx/boilerplate/parse.py
@@ -1,3 +1,16 @@
+#    Copyright 2023 Stanford University Convex Optimization Group
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
 import codecs
 
 from jinja2 import FileSystemLoader, Environment
@@ -8,9 +21,11 @@ def toml_data(toml_file="pyproject.toml"):
     d = toml.load(toml_file)
     return d["tool"]["poetry"]
 
+
 def jinja_environment(folder):
     environment = Environment(loader=FileSystemLoader(folder))
     return environment
+
 
 def write(template, output_file, **kwargs):
     # output the file
