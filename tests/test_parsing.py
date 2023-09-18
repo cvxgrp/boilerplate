6266c0dcf20a6bc4ce85d88bd9a5e6053b0a3955
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-09-18 08:10:34 -0700

    test environment

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -1,4 +1,5 @@
-from cvx.boilerplate.parse import toml_data
+from cvx.boilerplate.parse import toml_data, jinja_environment
+from jinja2 import environment
 
 
 def test_load_data(resource_dir):
@@ -6,3 +7,8 @@ def test_load_data(resource_dir):
     assert d["name"] == "test"
     assert d["authors"][0] == "Peter Maffay"
     assert d["packages"][0]["include"] == "cvx"
+
+
+def test_environment(resource_dir):
+    env = jinja_environment(resource_dir / "docs")
+    assert isinstance(env, environment.Environment)
