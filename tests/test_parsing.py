42446d5eaf602b4fabd991d2fd0c51304f5f3eb2
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-01-07 20:02:06 +0400

    towards uv

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -8,7 +8,6 @@ def test_load_data(resource_dir):
     d = toml_data(resource_dir / "data.toml")
     assert d["name"] == "test"
     assert d["authors"][0] == "Peter Maffay"
-    assert d["packages"][0]["include"] == "cvx"
 
 
 def test_environment(resource_dir):
